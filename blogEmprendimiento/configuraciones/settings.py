8cef83b588a17102b2004ba01d0435abfa8a84ea
LucasDRavarotto <lucasravarotto27@hotmail.com>
2024-10-12 19:06:29 -0300

    first commit

--- blogEmprendimiento/configuraciones/settings.py
+++ blogEmprendimiento/configuraciones/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$%_azx0@nan3o)y)2h)(@gq&e8+*1w9e)f$q2^=2il4i^dhyft'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR),'static'),)
 
 # Default primary key field type
