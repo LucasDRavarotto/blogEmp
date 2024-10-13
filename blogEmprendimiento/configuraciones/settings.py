284eab5ff594f8f8d966e40f7b55a174f8117483
LucasDRavarotto <lucasravarotto27@hotmail.com>
2024-10-13 18:53:18 -0300

    Solucion Admin

--- blogEmprendimiento/configuraciones/settings.py
+++ blogEmprendimiento/configuraciones/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$%_azx0@nan3o)y)2h)(@gq&e8+*1w9e)f$q2^=2il4i^dhyft'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -118,8 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR),'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
